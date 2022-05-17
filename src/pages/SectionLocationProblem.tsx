12db22d19a54c68022dda41d6d23b92e54f2379c
WittevrongelLaura <laura.wittevrongel@student.howest.be>
2022-05-17 10:27:48 +0200

    feat: sectionpage

---
+++ src/pages/SectionLocationProblem.tsx
@@ -0,0 +1,13 @@
+// Dit is de eerste sectie na de header van de ambitionpage
+// wat is de huidige situatie
+// wat is het probleem
+// copy paste dit naar de main ambitionpage
+
+import React from 'react'
+import {Text} from 'react-native'
+
+export default () => {
+    return (
+        <Text>Wat is de huidige situatie in </Text>
+    )
+}
