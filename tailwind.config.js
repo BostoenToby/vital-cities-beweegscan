7af56f78680a920ddd37f27b180ac33372259232
Toby <toby.bostoen@student.howest.be>
2022-05-17 09:28:28 +0200

    custom colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,13 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    colors: {
+      'pink': "#e7358b",
+      'purple': "#492784",
+      'yellow': "#f2c123",
+      'white': '#ffffff',
+      'black': '#000000'
+    }
   },
   plugins: [],
 }
