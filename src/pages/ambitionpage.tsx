3ba033887bf76daf9cd684da039e55bfcce67019
Toby <toby.bostoen@student.howest.be>
2022-05-18 11:52:05 +0200

    ambition page basic styling

--- src/pages/ambitionpage.tsx
+++ src/pages/ambitionpage.tsx
@@ -3,6 +3,7 @@ import { StaticImage } from "gatsby-plugin-image"
 import scrollTo from "gatsby-plugin-smoothscroll"
 import { ArrowDown, ExternalLink, FileText } from "lucide-react"
 import * as React from "react"
+import DonutChart from "../components/donutChart"
 
 function AmbitionPage () {
   return (
@@ -35,6 +36,111 @@ function AmbitionPage () {
           <StaticImage src="../images/Twitter.png" alt="Twitter" className="absolute right-8 top-[410px]" />
           <StaticImage src="../images/Instagram.png" alt="Instagram" className="absolute right-8 top-[440px]" />
       </header>
+
+      <section className="flex font-poppins mx-20 mt-32 gap-28">
+        <div className="flex flex-col max-w-xl pr-10">
+            <label className="text-3xl font-black">
+                Wat is de huidige situatie in
+            </label>
+            <select className="text-3xl font-black text-purple -ml-1 underline underline-offset-4">
+                <option>het Vlaams gewest</option>
+            </select>
+            <label className="mt-5 font-medium">
+                In het Vlaams gewest is <span className="font-semibold">ongeveer de helft of meer van de
+                inwoners</span> <span className="text-pink font-semibold">niet tevreden</span> over de staat, veiligheid en
+                aantrekkelijkheid van straten, pleinen, wandel- en fietspaden
+                (dus een samenvatting van alle cijfers). 
+            </label>
+        </div>
+        <div className="self-end">
+            <label className="opacity-50 text-sm font-black font-mono pl-4">
+                HOEVEEL % VAN DE INWONERS IS NIET TEVREDEN OVER ... 
+            </label>
+            <table>
+                <tr>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={44} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Staat straten & pleinen</label>
+                        <label className="text-pink font-bold">44%</label>
+                    </div>
+                    </div>
+                </td>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={54} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Staat voetpaden</label>
+                        <label className="text-pink font-bold">54%</label>
+                    </div>
+                    </div>
+                </td>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={59} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Staat fietspaden</label>
+                        <label className="text-pink font-bold">59%</label>
+                    </div>
+                    </div>
+                </td>
+                </tr>
+
+                <tr>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={55} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Genoeg fietspaden</label>
+                        <label className="text-pink font-bold">55%</label>
+                    </div>
+                    </div>
+                </td>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={60} />
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Fietsinfrastructuur</label>
+                        <label className="text-pink font-bold">60%</label>
+                    </div>
+                    </div>
+                </td>
+                <td className="p-2">
+                    <div className="flex items-center">
+                    <div className="w-12 h-12 mb-2">
+                        <DonutChart percentage={57}/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-sm font-medium">Veilig fietsen</label>
+                        <label className="text-pink font-bold">57%</label>
+                    </div>
+                    </div>
+                </td>
+                </tr>
+            </table>
+        </div>
+    </section>
+    <section className="flex flex-col items-center font-poppins mt-32 mx-20 mb-32">
+        <label className="text-3xl font-black mb-5">Wat is het probleem?</label>
+        <p className="mb-5">
+            Als routes geen goede verbinding maken met voorzieningen en werk of school, als ze onveilig zijn of door een weinig aantrekkelijke
+            stadsomgeving gaan, zijn mensen niet geneigd om ze te gebruiken. Als je fiets of wandelt, voel je je namelijk kwetsbaarder dan in je auto. 
+        </p>
+        <p className="font-black">
+            Bij een gebrek aan veilige en/of aantrekkelijke routes zullen mensen dan eerder kiezen voor de auto.
+        </p>
+    </section>
 
       <section className="mx-16 mb-16">
         <h2 className="text-3xl font-bold mb-4"><span className="underline decoration-lightPurple">Waarom</span> moeten we dit oplossen?</h2>
