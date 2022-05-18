d18671be269c9ca44289084035ed41edd33b97d0
Ian Van Cauwenberg <ian.van.cauwenberg@student.howest.be>
2022-05-18 10:21:55 +0200

    html structuur

--- src/pages/overviewpage.tsx
+++ src/pages/overviewpage.tsx
@@ -1,6 +1,41 @@
 import React from 'react'
 import '../assets/tailwind.css'
+import Logo from '../components/Logo'
 
 export default () => {
-    <main></main>
+  return (
+    <div>
+      <nav className="h-24 w-full">
+        <div className="h-full w-32">
+          <div className="h-16 w-32">
+            <Logo />
+          </div>
+        </div>
+        <ul>
+          <li>
+            <a>Home</a>
+          </li>
+          <li>
+            <a>Beweegscan</a>
+          </li>
+          <li>
+            <a>Onze cases</a>
+          </li>
+          <li>
+            <a>Over ons</a>
+          </li>
+        </ul>
+        <button>Contacteer ons</button>
+      </nav>
+      <main>
+        <header>
+          <h1></h1>
+          <select></select>
+        </header>
+        {/* // grid */}
+        <div>{/* // content components mapped*/}</div>
+      </main>
+      <footer>{/* // footer wanneer die er is */}</footer>
+    </div>
+  )
 }
