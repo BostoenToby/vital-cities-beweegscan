441c33d7dd68dd542d91d2e1b7f9cdba5a3fa9f2
Ian Van Cauwenberg <ian.van.cauwenberg@student.howest.be>
2022-05-20 09:03:54 +0200

    Select styling

--- src/pages/overviewpagepractices.tsx
+++ src/pages/overviewpagepractices.tsx
@@ -5,12 +5,16 @@ import { Link } from 'gatsby'
 import { testJSON } from '../data/testPractices'
 import PracticeCard from '../components/PracticeCard'
 import TestPractice from '../interfaces/testPractice'
+import { ChevronDown } from 'lucide-react'
 
 export default () => {
-  const [practices, setPractices] = useState<TestPractice[]>()
+  const [originalPractices, setOriginalPractices] = useState<TestPractice[]>()
+  const [currentPractices, setCurrentPractices] = useState<TestPractice[]>()
+  const [selected, setSelected] = useState<string>()
 
   useEffect(() => {
-    setPractices(testJSON)
+    setOriginalPractices(testJSON)
+    setCurrentPractices(testJSON)
   }, [])
 
   // useEffect(() => {
@@ -18,6 +22,30 @@ export default () => {
   //     console.log(practices)
   //   }
   // }, [practices])
+
+  useEffect(() => {
+    if (selected && originalPractices && originalPractices.length >= 1) {
+      console.log(selected)
+      let practices = originalPractices
+      let result: TestPractice[] = []
+
+      if (selected !== 'alle practices') {
+        practices.forEach((p) => {
+          if (p.themas.includes(selected)) {
+            result.push(p)
+          }
+        })
+      } else {
+        result = practices
+      }
+
+      setCurrentPractices(result)
+    }
+  }, [selected])
+
+  const handleSelect = (e: any) => {
+    setSelected(e.target.value)
+  }
 
   return (
     <div>
@@ -28,28 +56,38 @@ export default () => {
             Good practices
           </h1>
           <div className="my-auto">
-            <select className="font-poppins text-2xl font-semibold text-purple">
-              <option value="actief bewegen">
-                Actief bewegen & verplaatsen
-              </option>
-              <option value="verbonden stadkern">Verbonden stadskern</option>
-              <option value="fiets- en wandelroutes">
-                Fiets- & wandelroutes
-              </option>
-              <option value="sporten">Sporten</option>
-              <option value="spelen">Spelen</option>
-              <option value="ontmoeten">Ontmoeten</option>
-              <option value="groen">Groen</option>
-              <option value="technologie/data">Technologie & data</option>
-              <option value="transversaal">Transversaal beleid</option>
-              <option value="participatie">Participatie & inspraak</option>
-            </select>
+            <span className="group relative block">
+              {/* note: I hate select  */}
+              <select
+                value={selected}
+                onChange={(event) => handleSelect(event)}
+                className="appearance-none border-none p-2 pb-8 pr-8 font-poppins text-2xl font-semibold text-dark text-opacity-70 outline-none group-focus-within:text-purple group-active:text-purple"
+              >
+                <option value="alle practices">Alle practices</option>
+                <option value="actief bewegen">
+                  Actief bewegen & verplaatsen
+                </option>
+                <option value="verbonden stadskern">Verbonden stadskern</option>
+                <option value="fiets- en wandelroutes">
+                  Fiets- & wandelroutes
+                </option>
+                <option value="sporten">Sporten</option>
+                <option value="spelen">Spelen</option>
+                <option value="ontmoeten">Ontmoeten</option>
+                <option value="groen">Groen</option>
+                <option value="technologie/data">Technologie & data</option>
+                <option value="transversaal">Transversaal beleid</option>
+                <option value="participatie">Participatie & inspraak</option>
+              </select>
+              <div className="absolute top-12 h-1 w-full bg-purple bg-opacity-30 group-focus-within:bg-opacity-75 group-active:bg-opacity-75"></div>
+              <ChevronDown className="opacity-7 pointer-events-none absolute right-0 top-3 text-dark" />
+            </span>
           </div>
         </header>
         {/* // grid */}
-        {practices && practices.length >= 1 ? (
+        {currentPractices && currentPractices.length >= 1 ? (
           <div className="gap grid grid-cols-3 gap-x-14 gap-y-20">
-            {practices.map((e, i) => (
+            {currentPractices.map((e, i) => (
               <PracticeCard practice={e} key={e.id} />
             ))}
           </div>
