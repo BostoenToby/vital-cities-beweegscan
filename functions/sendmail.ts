bb55489cb05f6cb01d924fb15c172baae0aa2632
Toby <toby.bostoen@student.howest.be>
2022-06-02 19:52:44 +0200

    test if netlify functions works

--- functions/sendmail.ts
+++ functions/sendmail.ts
@@ -1,33 +1,3 @@
-import { builder, Handler } from '@netlify/functions'
-
-const myHandler: Handler = async (event, context) => {
-  const sgMail = require('@sendgrid/mail');
-  console.log("This is a test for the function")
-  console.log(process.env.SENDGRID_API_KEY)
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const data = JSON.parse(String(event.body))
-  const { email, subject } = data
-  const body = Object.keys(data).map((k) => {
-    return `${k}: ${data[k]}`
-  }).join("<br><br>");
-  const mail_to_send = {
-    to: email,
-    from: "toby.bostoen@student.howest.be",
-    subject: subject ? subject : 'New Entry from Contact Form',
-    html: body
-  };
-  try{
-    await sgMail.send(mail_to_send)
-    return {
-      statusCode: 200,
-      body: "Message sent successfully"
-    }
-  } catch(e: any){
-    return {
-      statusCode: e.code,
-      body: e.message
-    }
-  }
-};
-
-exports.handler = builder(myHandler)
+exports.handler = async function(event: any, context: any){
+  console.log("test function")
+}
