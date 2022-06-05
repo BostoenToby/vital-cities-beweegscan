53f9e60a984e03e4fc2d3dc535a512240df67c0f
Toby <toby.bostoen@student.howest.be>
2022-06-05 14:58:44 +0200

    testing because error at JSON.parse

--- functions/sendmail.ts
+++ functions/sendmail.ts
@@ -3,19 +3,12 @@ sgMail3.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.handler = async (event: any, context: any, callback: any) => {
     console.log("Testing if this works")
-    const data = JSON.parse(event.body)
-    // const { email, subject } = data
-
-    const body = Object.keys(data).map((k) => {
-        return `${k}: ${data[k]}`
-      }).join("<br><br>");
 
     const mail_to_send = {
         to: "toby.bostoen@student.howest.be",
         from: "toby.bostoen@student.howest.be",
         subject: 'This is a test',
-        text: 'This was not easy at all',
-        html: body
+        text: 'This was not easy at all'
     }
 
     try{
