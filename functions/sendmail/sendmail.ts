b0e6b84d0bae68b1cd00244e8c739116ecad3792
Toby <toby.bostoen@student.howest.be>
2022-06-05 19:19:00 +0200

    possible fix for bad request

---
+++ functions/sendmail/sendmail.ts
@@ -0,0 +1,41 @@
+const sgMail2 = require('@sendgrid/mail');
+sgMail2.setApiKey(process.env.SENDGRID_API_KEY);
+
+exports.handler = async (event: any, context: any, callback: any) => {
+    console.log("Testing if this works")
+    // const body = JSON.parse(event.body)
+    // const message = body.message
+    // const attachement = 
+
+    const mail_to_send = {
+        personalizations: [
+            {
+               cc: [
+                   {
+                       email: 'sigofoy785@game4hr.com',
+                       name: 'testCC'
+                   }
+               ] 
+            }
+        ],
+        to: "toby.bostoen@student.howest.be",
+        from: "toby.bostoen@student.howest.be",
+        subject: 'This is a test',
+        text: "This is a new test with axios and just fetch",
+        html: '<strong>This was not easy at all</strong>',
+    }
+
+    try{
+        await sgMail2.send(mail_to_send).then(() => console.log("Email sent!!!"))
+
+        return{
+            statusCode: 200,
+            body: "Message sent successfully"
+        }
+    } catch(e: any){
+        return{
+            statusCode: e.code,
+            body: e.message
+        }
+    }
+}
