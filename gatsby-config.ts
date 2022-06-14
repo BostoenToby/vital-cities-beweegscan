2a432c8dba333fa874c511f9e76ba4a1ffc0228f
Toby <toby.bostoen@student.howest.be>
2022-06-14 10:53:46 +0200

    trying script-src unsafe-inline

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -90,7 +90,7 @@ const config: GatsbyConfig = {
         mergeStyleHashes: false, // you can disable styles sha256 hashes
         mergeDefaultDirectives: true,
         directives: {
-          "script-src": "'self' www.google-analytics.com",
+          "script-src": "'self' 'unsafe-inline'",
           "style-src": "'self' 'unsafe-inline'",
           "img-src": "'self' data: www.google-analytics.com"
           // you can add your directives or override defaults
